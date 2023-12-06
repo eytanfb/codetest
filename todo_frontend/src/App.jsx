7bd0c72aea3b33deb98e03c24abd54639b4296df
Eytan Anjel <eytanfb@gmail.com>
2023-12-06 13:13:36 -0800

    setting up basic styles with dummy content

--- todo_frontend/src/App.jsx
+++ todo_frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './stylesheets/App.scss'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        Hello
+    <div className="w-screen h-screen p-16 border-2 border-gray-300 bg-background">
+      <div className="flex items-center justify-center h-20 shadow-lg bg-header-background">
+        <h1 className="text-3xl font-bold text-header-text">Just Do It</h1>
       </div>
-    </>
+
+      <div className="flex flex-col w-full p-4 h-96">
+        <div className="flex w-full p-4">
+          <input type="checkbox" />
+          <div className="ml-4 text-xl hover:cursor-text">This is a todo item</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
