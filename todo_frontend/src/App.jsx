481512c1a8be5c6db0e5dfa80ad06629b63e297e
Eytan Anjel <eytanfb@gmail.com>
2023-12-06 12:26:43 -0800

    setting up frontend app using vite. added tailwindcss for styling and jest for testing. added some colors for roaming hunger theme.

---
+++ todo_frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './stylesheets/App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ todo_frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        background: '#F8F8F8',
+        success: '#1BB588',
+        danger: '#E91D25',
+        warning: '#FDD213',
+        header: {
+          text: '#87D4F6',
+          background: '#1E71B2',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
+
