338e8607b19da6393e6948e819873893032a62e6
Eytan Anjel <eytanfb@gmail.com>
2023-12-06 17:23:22 -0800

    added error handling and some more styling with fontawesome

--- todo_frontend/src/components/PlaceholderTodo.tsx
+++ todo_frontend/src/components/PlaceholderTodo.tsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 import { Todo as TodoType } from "../api/types/Todo";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
   todo: TodoType;
@@ -11,6 +13,7 @@ const PlaceholderTodo = ({ todo, onToggleComplete, onDescriptionChange }: Props)
   const [isChecked, setIsChecked] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [description, setDescription] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     setIsChecked(todo.done);
@@ -23,7 +26,13 @@ const PlaceholderTodo = ({ todo, onToggleComplete, onDescriptionChange }: Props)
   }
 
   const onUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDescription(event.target.value);
+    const description = event.target.value;
+
+    if (description.length > 0) {
+      setError('');
+    }
+
+    setDescription(description);
   };
 
   const onClick = () => {
@@ -31,9 +40,14 @@ const PlaceholderTodo = ({ todo, onToggleComplete, onDescriptionChange }: Props)
   }
 
   const onSave = () => {
+    if (description === '') {
+      setError('Description cannot be empty');
+      return;
+    }
     onDescriptionChange(description);
     setIsEditing(false);
     setDescription('');
+    setError('');
   }
 
   const displayClasses = isChecked ? 'ml-4 text-xl line-through text-success' : 'ml-4 text-xl font-bold hover:cursor-text';
@@ -41,10 +55,19 @@ const PlaceholderTodo = ({ todo, onToggleComplete, onDescriptionChange }: Props)
 
   return (
     <div key={todo.id} className={`flex w-full p-4 ${opacityClasses}`}>
-      <input type="checkbox" checked={isChecked} onChange={onChange} data-testid={`todo-${todo.id}-checkbox`} />
+      <input type="checkbox" disabled checked={isChecked} onChange={onChange} data-testid={`todo-${todo.id}-checkbox`} />
         <div className="flex justify-between w-full">
-          <input placeholder="Click to add new todo" type="text" className="flex-grow pl-4 ml-4 text-xl font-bold bg-background" onClick={onClick} onChange={onUpdate} defaultValue={todo.description} data-testid={`todo-${todo.id}-input`} value={description} />
-          { isEditing && <button className="h-8 pl-8 ml-4 text-xl font-bold text-white rounded-full w-7 bg-success text-success" onClick={onSave} data-testid={`todo-${todo.id}-save`}></button> }
+          <div className="flex flex-col w-full">
+            <input placeholder="Click to add new todo" type="text" className="flex-grow pl-4 ml-4 text-xl font-bold bg-background" onClick={onClick} onChange={onUpdate} data-testid={`todo-${todo.id}-input`} value={description} />
+            { error && <span className="ml-8 text-xs text-danger">{error}</span> }
+          </div>
+          { isEditing &&
+            <>
+              <button className="flex items-center justify-center w-8 h-8 ml-4 text-xl font-bold text-white rounded-full bg-success" onClick={onSave} data-testid={`todo-${todo.id}-save`}>
+                <FontAwesomeIcon icon={faCheck} size="xs" />
+              </button>
+            </>
+          }
         </div>
     </div>
   );
--- todo_frontend/src/components/Todo.tsx
+++ todo_frontend/src/components/Todo.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Todo as TodoType } from "../api/types/Todo";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
   todo: TodoType;
@@ -12,6 +14,7 @@ const Todo = ({ todo, onToggleComplete, onDescriptionChange, onDelete }: Props):
   const [isChecked, setIsChecked] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [description, setDescription] = useState('');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     setIsChecked(todo.done);
@@ -32,6 +35,10 @@ const Todo = ({ todo, onToggleComplete, onDescriptionChange, onDelete }: Props):
   }
 
   const onSave = () => {
+    if (description === '') {
+      setError('Description cannot be empty');
+      return;
+    }
     onDescriptionChange(todo.id, description);
     setIsEditing(false);
   }
@@ -43,18 +50,22 @@ const Todo = ({ todo, onToggleComplete, onDescriptionChange, onDelete }: Props):
   const displayClasses = isChecked ? 'ml-4 text-xl line-through text-success' : 'ml-4 text-xl font-bold hover:cursor-text';
 
   return (
-    <div key={todo.id} className="flex w-full p-4">
+    <div key={todo.id} className={`flex w-full p-4 ${isChecked ? 'opacity-30' : ''}`}>
       <input type="checkbox" checked={isChecked} onChange={onChange} data-testid={`todo-${todo.id}-checkbox`} />
       { isEditing &&
         <div className="flex justify-between w-full">
-          <input type="text" autoFocus className="flex-grow pl-4 ml-4 text-xl font-bold" onChange={onUpdate} defaultValue={todo.description} data-testid={`todo-${todo.id}-input`} />
-          <button className="h-8 pl-8 ml-4 text-xl font-bold rounded-full w-7 bg-success" onClick={onSave} data-testid={`todo-${todo.id}-save`}></button>
+          <input type="text" autoFocus className="flex-grow pl-4 ml-4 text-xl font-bold" onChange={onUpdate} value={description} data-testid={`todo-${todo.id}-input`} />
+          <button className="w-8 h-8 ml-4 text-xl font-bold text-white rounded-full bg-success" onClick={onSave} data-testid={`todo-${todo.id}-save`}>
+            <FontAwesomeIcon icon={faCheck} size="xs" />
+          </button>
         </div>
       }
       { !isEditing &&
           <div className="flex justify-between w-full">
             <div className={displayClasses} onClick={onClick} data-testid={`todo-${todo.id}-input`}>{todo.description}</div>
-            <button className="h-8 pl-8 ml-4 text-xl font-bold rounded-full w-7 bg-danger" onClick={deleteTodo} data-testid={`todo-${todo.id}-delete`}></button>
+            <button className="flex items-center justify-center w-8 h-8 ml-4 text-xl font-bold text-white rounded-full bg-danger" onClick={deleteTodo} data-testid={`todo-${todo.id}-delete`}>
+              <FontAwesomeIcon icon={faTrash} size="xs" />
+            </button>
           </div>
       }
     </div>
